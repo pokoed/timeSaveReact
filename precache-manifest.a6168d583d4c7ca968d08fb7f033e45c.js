65762b515e67aef78ed148a1e446b17c3909de54
pokoed <20041230andy@gmail.com>
2020-09-23 22:59:19 +0900

    Updates

---
+++ precache-manifest.a6168d583d4c7ca968d08fb7f033e45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb434e9947aa934ec7e8b7e36a1a894",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b09ac0731589ec21c721",
+    "url": "/static/css/main.3d378a6f.chunk.css"
+  },
+  {
+    "revision": "3e6d40c4c44ef554ab7c",
+    "url": "/static/js/2.95ed80f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95ed80f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09ac0731589ec21c721",
+    "url": "/static/js/main.022058ee.chunk.js"
+  },
+  {
+    "revision": "95491ed70cf1dc2a794e",
+    "url": "/static/js/runtime-main.47256219.js"
+  }
+]);
